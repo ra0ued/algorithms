92973e6aab2ffe8490226098206582ce2de561b5
ra0ued <ra0ued@zabtech.ru>
2018-10-04 23:59:53 +0900

    Improvement.

--- DataProviderImprovement.php
+++ DataProviderImprovement.php
@@ -13,10 +13,19 @@ interface Provider
 
 interface Request
 {
+    public function toArray();
 }
 
 interface Response
 {
+}
+
+class HTMLResponse implements Response
+{
+    public function toArray()
+    {
+        return [];
+    }
 }
 
 namespace src\App\Provider;
@@ -54,13 +63,77 @@ class DataProvider implements Provider
      */
     public function get(Request $request): Response
     {
-        // Some implementation
+        $response = new HTMLResponse();
+
+        return $response;
     }
 }
 
 namespace src\App\Provider;
 
-class CacheDataProvider extends DataProvider
+use DateTime;
+use Psr\Cache\CacheItemPoolInterface;
+use Psr\Log\LoggerInterface;
+
+class CacheDataProvider implements Provider
 {
-    // Or not...
+    /**
+     * @var Provider
+     */
+    private $provider;
+
+    /**
+     * @var CacheItemPoolInterface
+     */
+    private $cache;
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    public function __construct(Provider $provider, LoggerInterface $logger, CacheItemPoolInterface $cache)
+    {
+        $this->provider = $provider;
+        $this->cache = $cache;
+        $this->logger = $logger;
+    }
+
+    /**
+     * @param Request $request
+     * @return Response
+     */
+    public function get(Request $request): Response
+    {
+        try {
+            $cacheKey = $this->getCacheKey($request->toArray());
+            $cacheItem = $this->cache->getItem($cacheKey);
+            if ($cacheItem->isHit()) {
+                return $cacheItem->get();
+            }
+
+            $result = $this->provider->get($request);
+
+            $cacheItem
+                ->set($result)
+                ->expiresAt(
+                    (new DateTime())->modify('+1 day')
+                );
+
+            return $result;
+        } catch (\Throwable $e) {
+            $this->logger->critical($e->getMessage());
+
+            return new HTMLResponse();
+        }
+    }
+
+    /**
+     * @param array $input
+     * @return string
+     */
+    private function getCacheKey(array $input)
+    {
+        return md5(json_encode($input));
+    }
 }
